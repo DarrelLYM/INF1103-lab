5fba056264ccd0518b3d91b4f23c02fcadf6949d
your_email@example.com <darrellimym@gmail.com>
2026-09-04 17:27:20 +0800

    First Post

--- hello.py
+++ hello.py
@@ -15,8 +15,20 @@ print("Day 2:", followers)
 
 followers -= 10
 print("Day 3:", followers)
-      
 
+      
+# print("Username:", username)
+# print("Bio:", bio)
+# print("Followers:", followers)
+
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("/nInstagram Profile")
+print ("=====================")
 print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+print("Age:", age)
+print("Category:", category)
+
+
